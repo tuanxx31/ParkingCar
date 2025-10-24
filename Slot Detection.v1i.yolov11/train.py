9ccde833939f5ef71e2cc9192e61745a8879cd6d
tuanxx31 <225748020110138@vinhuni.edu.vn>
2025-10-24 21:42:03 +0700

    okok

--- Slot Detection.v1i.yolov11/train.py
+++ Slot Detection.v1i.yolov11/train.py
@@ -4,14 +4,13 @@ def main():
     model = YOLO("yolo11m.pt")
     model.train(
         data="data.yaml",
-        epochs=10,
+        epochs=50,
         imgsz=640,
         batch=32,
         device=0,     # GPU RTX 3060
         workers=4,    # fix thêm để tránh lỗi spawn
         cache=True,
         amp=True
-        # lr0=0.001
     )
 
 if __name__ == "__main__":
