2b3d57d13bd74795652fde5f4a5a74b29f816e6b
tuanxx31 <225748020110138@vinhuni.edu.vn>
2025-10-24 18:57:36 +0700

    Update main.py to improve object detection visualization. Changed model loading from 'best.py' to 'best.pt', added bounding box drawing and class name display for detected objects, and removed test.py as it was redundant.

--- main.py
+++ main.py
@@ -3,13 +3,24 @@ from ultralytics import YOLO
 
 img_path = "ParkingCar_Data/CarParkProject/carParkImg.png"
 
-model = YOLO("best.py")
+model = YOLO("best.pt")
 
 results = model(img_path)
 names = model.names
+
 for result in results:
+    img = result.orig_img.copy()
     boxes = result.boxes
     for box in boxes:
-        x1, y1, x2, y2 = box
-        class_id = box.cls.int()
-        
+        if box.conf >0.5:
+            x1, y1, x2, y2 = map(int,box.xyxy[0])
+            class_id = box.cls.item()
+            class_name = names[class_id]
+            print(f"Class: {class_name}, Bounding Box: ({x1}, {y1}, {x2}, {y2})")
+            color = (0,255,0)
+            cv2.rectangle(img,(x1,y1),(x2,y2),color)
+            cv2.putText(img,class_name,(x1,y1),cv2.FONT_HERSHEY_SIMPLEX,0.7,color)
+
+cv2.imshow("show",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
