5096ced7e7da7bdf347eb76aceee3139bf2aca08
tuanxx31 <225748020110138@vinhuni.edu.vn>
2025-11-01 17:30:36 +0700

    ok

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import cv2
 import math
 
 def draw_rectangle(img, top_left, bottom_right, color=(0, 255, 0), thickness=2, label=None, conf=None):
-    """Vẽ hình chữ nhật, dùng cho cả Gate và Slot."""
     x1, y1 = top_left
     x2, y2 = bottom_right
 
@@ -15,12 +14,10 @@ def draw_rectangle(img, top_left, bottom_right, color=(0, 255, 0), thickness=2, 
 
 
 def get_center_rectangle(top_left, bottom_right):
-    """Tính tâm của hình chữ nhật."""
     x1, y1 = top_left
     x2, y2 = bottom_right
     return int((x1 + x2) / 2), int((y1 + y2) / 2)
 
 
 def calc_distance(p1, p2):
-    """Tính khoảng cách Euclidean giữa 2 điểm."""
     return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
