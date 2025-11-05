a536b521fcf778d3abf6b10cc7c1dc86faf92e47
Thuy0810 <Tothuy0810@gmail.com>
2025-11-05 12:22:06 +0700

    ok

--- detect_image.py
+++ detect_image.py
@@ -1,19 +1,25 @@
 import cv2
 from ultralytics import YOLO
 from conf import colors
-from utils import draw_rectangle
+from utils import draw_rectangle, get_center_rectangle, calc_distance
 
 
-def detect_image(img_path, model_path="model/v5/best.pt", threshold=0.25):
+
+def detect_image(img_path, model_path="model/v5/best.pt", threshold=0.25, gate_rect_image=None):
     model = YOLO(model_path)
     names = model.names
 
     results = model(img_path)
     empty_count = 0
+    nearest_slot = None
+    min_distance = float("inf")
+    gate_center = None
+    if gate_rect_image:
+        gate_center = get_center_rectangle(*gate_rect_image)
 
     for result in results:
         boxes = result.boxes
-        frame = result.orig_img.copy()
+        image = result.orig_img.copy()
 
         for box in boxes:
             class_id = int(box.cls.item())
@@ -24,11 +30,36 @@ def detect_image(img_path, model_path="model/v5/best.pt", threshold=0.25):
                 empty_count += 1
 
                 x1, y1, x2, y2 = map(int, box.xyxy[0])
-                draw_rectangle(frame, x1, y1, x2, y2, color=colors[class_id], label=class_name, conf=conf)
+                slot_top_left = (x1, y1)
+                slot_bottom_right = (x2, y2)
 
-    cv2.putText(frame, f"Empty : {empty_count}", (10, 30),
+                slot_center = get_center_rectangle(slot_top_left, slot_bottom_right)
+                if gate_center:
+                    dist = calc_distance(slot_center, gate_center)
+                    if dist < min_distance:
+                        min_distance = dist
+                        nearest_slot = (slot_top_left, slot_bottom_right, slot_center)
+
+                draw_rectangle(image, slot_top_left, slot_bottom_right,  color=colors[class_id], label=class_name, conf=conf)
+
+                
+        if(gate_rect_image):
+            draw_rectangle(image, gate_rect_image[0], gate_rect_image[1],
+                           color=(255, 0, 0),
+                           label="Gate")
+            if gate_center:
+                cv2.circle(image, gate_center, 6, (255, 0, 0), -1)
+
+        if nearest_slot:
+            slot_top_left, slot_bottom_right, slot_center = nearest_slot
+            draw_rectangle(image, slot_top_left, slot_bottom_right,
+                           color=(0, 255, 255),
+                           label="Nearest")
+            cv2.circle(image, slot_center, 6, (0, 255, 255), -1)
+
+    cv2.putText(image, f"Empty : {empty_count}", (10, 30),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-    cv2.imshow("Parking Detection", frame)
+    cv2.imshow("Parking Detection", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from detect_video import detect_video
 
 model_path = "model/v5/best.pt"
 video_path = "test/carPark.mp4"
-import cv2
+image_path = r"test\0.png"
 
 
 def select_gate(video_path):
@@ -30,8 +30,26 @@ def select_gate(video_path):
     print(gate_rect)
     return gate_rect
 
+def slect_gate_image(image_path):
+    img= cv2.imread(image_path)
+    roi = cv2.selectROI("Chọn vùng cổng (Enter để xác nhận)", img)
+    cv2.destroyAllWindows()
+    x, y, w, h = roi
+    if w == 0 or h == 0:
+        print("Chưa chọn vùng nào.")
+        return None
+    top_left = (int(x), int(y))
+    bottom_right = (int(x + w), int(y + h))
+    gate_rect = [top_left, bottom_right]
+    print(gate_rect)
+    return gate_rect
+
 if __name__ == "__main__":
     # detect_image("test/0.png",model_path,0.4)
     # detect_video("test/testVideo.mp4",model_path,0.2)
-    gate_rect = select_gate(video_path)
-    detect_video("test/carPark.mp4", model_path, 0.2, gate_rect)
+    # gate_rect = select_gate(video_path)
+    # detect_video("test/carPark.mp4", model_path, 0.2, gate_rect)
+
+    gate_rect_image = slect_gate_image(image_path)
+    detect_image(image_path, model_path, 0.2, gate_rect_image)
+   
